bc8906ffc96f911da0fe890b3d623b7d57c07654
Dachacho <arevadzedachi123@gmail.com>
2025-08-21 18:35:32 +0400

    feat: added songservice interface and implemented it into song service

---
+++ Services/ISongService.cs
@@ -0,0 +1,12 @@
+using MusicDiscoveryAPI.DTOs;
+
+namespace MusicDiscoveryAPI.Services {
+    public interface ISongService
+    {
+        Task<IEnumerable<SongDTO>> GetAllSongsAsync();
+        Task<SongDTO?> GetSongByIdAsync(int id);
+        Task<SongDTO> CreateSongAsync(SongCreateDTO dto);
+        Task<SongDTO> UpdateSongAsync(SongCreateDTO dto, int id);
+        Task<bool> DeleteSongAsync(int id);
+    }
+}
---
+++ Services/SongService.cs
@@ -0,0 +1,39 @@
+using MusicDiscoveryAPI.Data;
+using MusicDiscoveryAPI.DTOs;
+
+namespace MusicDiscoveryAPI.Services
+{
+    public class SongService : ISongService
+    {
+        private readonly MusicDiscoveryContext _context;
+        public SongService(MusicDiscoveryContext context)
+        {
+            _context = context;
+        }
+
+        public Task<SongDTO> CreateSongAsync(SongCreateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteSongAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SongDTO>> GetAllSongsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SongDTO?> GetSongByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SongDTO> UpdateSongAsync(SongCreateDTO dto, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
