e2bc9a9feca9a63a4fb1f306a0edef423c8ddc9d
Dachacho <arevadzedachi123@gmail.com>
2025-08-21 14:54:18 +0400

    feat: added a jwt token generator function

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,21 +3,72 @@ using MusicDiscoveryAPI.Models;
 using MusicDiscoveryAPI.DTOs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace MusicDiscoveryAPI.Services
 {
     public class UserService : IUserService
     {
-        public readonly MusicDiscoveryContext _context;
+        private readonly MusicDiscoveryContext _context;
+        private readonly IConfiguration _configuration;
 
-        public UserService(MusicDiscoveryContext context)
+        public UserService(MusicDiscoveryContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new[]{
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            var jwtKey = _configuration["Jwt:Key"];
+            var jwtIssuer = _configuration["Jwt:Issuer"];
+            var jwtAudience = _configuration["Jwt:Audience"];
+            if (string.IsNullOrEmpty(jwtKey))
+            {
+                throw new InvalidOperationException("JWT Key not present in config");
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtIssuer,
+                audience: jwtAudience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public async Task<string?> LoginUserAsync(UserLoginDTO dto)
         {
-            return null;
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Username == dto.Username);
+
+            if (user == null) return null;
+            var hasher = new PasswordHasher<User>();
+
+            var result = hasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password);
+
+            if (result == PasswordVerificationResult.Success)
+            {
+                return "true";
+            }
+            else
+            {
+                throw new ArgumentException("Non Valid Credentials");
+            }
+            ;
         }
 
         public async Task<UserDTO> CreateUserAsync(UserCreateDTO dto)
