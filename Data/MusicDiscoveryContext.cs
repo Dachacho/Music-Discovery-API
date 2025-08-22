58ae720a697c4b61a34cb20015c0e3f329251be9
Dachacho <arevadzedachi123@gmail.com>
2025-08-22 23:30:50 +0400

    chore: seed songs and migration for test data

--- Data/MusicDiscoveryContext.cs
+++ Data/MusicDiscoveryContext.cs
@@ -12,5 +12,23 @@ namespace MusicDiscoveryAPI.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Playlist> Playlists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Song>().HasData(
+                new Song { Id = 1, Title = "Imagine", Artist = "John Lennon", Album = "Imagine", Duration = "3:04", Genre = "Rock", ReleaseYear = "1971" },
+                new Song { Id = 2, Title = "Billie Jean", Artist = "Michael Jackson", Album = "Thriller", Duration = "4:54", Genre = "Pop", ReleaseYear = "1982" },
+                new Song { Id = 3, Title = "Bohemian Rhapsody", Artist = "Queen", Album = "A Night at the Opera", Duration = "5:55", Genre = "Rock", ReleaseYear = "1975" },
+                new Song { Id = 4, Title = "Shape of You", Artist = "Ed Sheeran", Album = "Divide", Duration = "3:53", Genre = "Pop", ReleaseYear = "2017" },
+                new Song { Id = 5, Title = "Lose Yourself", Artist = "Eminem", Album = "8 Mile", Duration = "5:26", Genre = "Hip-Hop", ReleaseYear = "2002" },
+                new Song { Id = 6, Title = "Rolling in the Deep", Artist = "Adele", Album = "21", Duration = "3:48", Genre = "Soul", ReleaseYear = "2010" },
+                new Song { Id = 7, Title = "Smells Like Teen Spirit", Artist = "Nirvana", Album = "Nevermind", Duration = "5:01", Genre = "Grunge", ReleaseYear = "1991" },
+                new Song { Id = 8, Title = "Blinding Lights", Artist = "The Weeknd", Album = "After Hours", Duration = "3:20", Genre = "Synthpop", ReleaseYear = "2019" },
+                new Song { Id = 9, Title = "Uptown Funk", Artist = "Mark Ronson ft. Bruno Mars", Album = "Uptown Special", Duration = "4:30", Genre = "Funk", ReleaseYear = "2014" },
+                new Song { Id = 10, Title = "Viva La Vida", Artist = "Coldplay", Album = "Viva la Vida or Death and All His Friends", Duration = "4:02", Genre = "Alternative", ReleaseYear = "2008" }
+            ); 
+        }
     }
 }
---
+++ Migrations/20250822192856_SeedSongs.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MusicDiscoveryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSongs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "Id", "Album", "Artist", "Duration", "Genre", "ReleaseYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Imagine", "John Lennon", "3:04", "Rock", "1971", "Imagine" },
+                    { 2, "Thriller", "Michael Jackson", "4:54", "Pop", "1982", "Billie Jean" },
+                    { 3, "A Night at the Opera", "Queen", "5:55", "Rock", "1975", "Bohemian Rhapsody" },
+                    { 4, "Divide", "Ed Sheeran", "3:53", "Pop", "2017", "Shape of You" },
+                    { 5, "8 Mile", "Eminem", "5:26", "Hip-Hop", "2002", "Lose Yourself" },
+                    { 6, "21", "Adele", "3:48", "Soul", "2010", "Rolling in the Deep" },
+                    { 7, "Nevermind", "Nirvana", "5:01", "Grunge", "1991", "Smells Like Teen Spirit" },
+                    { 8, "After Hours", "The Weeknd", "3:20", "Synthpop", "2019", "Blinding Lights" },
+                    { 9, "Uptown Special", "Mark Ronson ft. Bruno Mars", "4:30", "Funk", "2014", "Uptown Funk" },
+                    { 10, "Viva la Vida or Death and All His Friends", "Coldplay", "4:02", "Alternative", "2008", "Viva La Vida" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
